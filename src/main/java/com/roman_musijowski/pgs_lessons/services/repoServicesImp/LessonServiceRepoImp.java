1542a7fa7c62a75e8334244208aa15d2c1ddac1b
Roman Musijowski <rmusijowski@gmail.com>
2019-04-21 01:01:24 +0200

    fixed delete lessons

--- src/main/java/com/roman_musijowski/pgs_lessons/services/repoServicesImp/LessonServiceRepoImp.java
+++ src/main/java/com/roman_musijowski/pgs_lessons/services/repoServicesImp/LessonServiceRepoImp.java
@@ -3,24 +3,30 @@ package com.roman_musijowski.pgs_lessons.services.repoServicesImp;
 import com.roman_musijowski.pgs_lessons.commands.LessonForm;
 import com.roman_musijowski.pgs_lessons.converters.LessonFormToLesson;
 import com.roman_musijowski.pgs_lessons.models.Lesson;
+import com.roman_musijowski.pgs_lessons.models.User;
 import com.roman_musijowski.pgs_lessons.repositories.LessonRepository;
 import com.roman_musijowski.pgs_lessons.services.LessonService;
+import com.roman_musijowski.pgs_lessons.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class LessonServiceRepoImp implements LessonService {
 
     private LessonRepository lessonRepository;
     private LessonFormToLesson lessonFormToLesson;
+    private UserService userService;
 
     @Autowired
-    public LessonServiceRepoImp(LessonRepository lessonRepository, LessonFormToLesson lessonFormToLesson) {
+    public LessonServiceRepoImp(UserService userService, LessonRepository lessonRepository, LessonFormToLesson lessonFormToLesson) {
         this.lessonRepository = lessonRepository;
         this.lessonFormToLesson = lessonFormToLesson;
+        this.userService = userService;
     }
 
     @Override
@@ -54,7 +60,34 @@ public class LessonServiceRepoImp implements LessonService {
 
     @Override
     public void delete(Long id) {
+        System.out.println("Delete in service");
+
+        Lesson lesson = getById(id);
+        Set<User> users = lesson.getUsers();
+
+        //deleting lessons from users
+        users.forEach(user -> {
+            User user1 = userService.getById(user.getId());
+            System.out.println(user1.toString());
+
+            if (user1.getLessons().contains(lesson)){
+                user1.getLessons().remove(lesson);
+                System.out.println("Lesson removed");
+            }
+            userService.saveOrUpdate(user1);
+            System.out.println("User removed");
+        });
+
+        //deleting users from lessons
+        for (Iterator<User> iterator = lesson.getUsers().iterator(); iterator.hasNext();) {
+            User user = iterator.next();
+            System.out.println(iterator.toString());
+            iterator.remove();
+            System.out.println("Remove user from lesson!");
+        }
+
         lessonRepository.deleteById(id);
+        System.out.println("Lesson deleted!");
     }
 
     @Override
