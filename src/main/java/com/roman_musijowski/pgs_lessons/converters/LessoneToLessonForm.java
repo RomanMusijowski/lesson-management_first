2a48834a1a36a6d1fb956119ac4281887d01a13d
Roman Musijowski <rmusijowski@gmail.com>
2019-04-28 03:07:25 +0200

    added lessonToLessonForm converter

---
+++ src/main/java/com/roman_musijowski/pgs_lessons/converters/LessoneToLessonForm.java
@@ -0,0 +1,22 @@
+package com.roman_musijowski.pgs_lessons.converters;
+
+import com.roman_musijowski.pgs_lessons.commands.LessonForm;
+import com.roman_musijowski.pgs_lessons.models.Lesson;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LessoneToLessonForm implements Converter<Lesson, LessonForm> {
+    @Override
+    public LessonForm convert(Lesson lesson) {
+        LessonForm lessonForm = new LessonForm();
+
+        lessonForm.setLesson_id(lesson.getLesson_id());
+        lessonForm.setTitle(lesson.getTitle());
+        lessonForm.setDescription(lesson.getDescription());
+        lessonForm.setTeacherInfo(lesson.getTeacherInfo());
+        lessonForm.setDate(lesson.getDate());
+
+        return lessonForm;
+    }
+}
