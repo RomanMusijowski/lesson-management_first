a849ea2c477201727f3d149a08cc14c4b61d640d
Roman Musijowski <rmusijowski@gmail.com>
2019-05-05 19:22:47 +0200

    updated validators

--- src/test/java/com/roman_musijowski/pgs_lessons/commands/validators/LessonFormValidatorTest.java
+++ src/test/java/com/roman_musijowski/pgs_lessons/commands/validators/LessonFormValidatorTest.java
@@ -1,21 +1,26 @@
 package com.roman_musijowski.pgs_lessons.commands.validators;
 
 import com.roman_musijowski.pgs_lessons.commands.LessonForm;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.time.LocalDateTime;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class LessonFormValidatorTest {
 
     private Validator validator;
     private LessonForm lessonForm;
     private Errors errors;
 
-    @BeforeEach
+    @Before
     void setUp() {
         validator = new LessonFormValidator();
         lessonForm = new LessonForm();
--- src/test/java/com/roman_musijowski/pgs_lessons/commands/validators/UserFormValidatorTest.java
+++ src/test/java/com/roman_musijowski/pgs_lessons/commands/validators/UserFormValidatorTest.java
@@ -1,18 +1,24 @@
 package com.roman_musijowski.pgs_lessons.commands.validators;
 
 import com.roman_musijowski.pgs_lessons.commands.UserForm;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class UserFormValidatorTest {
 
     UserFormValidator validator;
     UserForm userForm;
     Errors errors;
 
-    @BeforeEach
+    @Before
     void setUp() {
         validator = new UserFormValidator();
         userForm = new UserForm();
